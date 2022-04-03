b05fcccc0f2d00ce544d75f53604e9fe79ac1b4e
gatesma <4775500+terekete@users.noreply.github.com>
2022-04-03 09:44:45 -0400

    chore: update images (#8)

--- cloudbuild/scripts/build_images.sh
+++ cloudbuild/scripts/build_images.sh
@@ -2,10 +2,9 @@
 
 set -e
 
-cat DIFF_IMAGES.txt | while read image_folder
-do
+cat DIFF_IMAGES.txt | while read image_folder; do
     version=$(cat "./images/$image_folder/VERSION")
-    image_tag="${CONTAINER_IMAGE_REGISTRY}/"${image_folder}":${version}-rc"
+    image_tag="${CONTAINER_IMAGE_REGISTRY}/"${image_folder}":rc"
     docker build -t "$image_tag" "./images/${image_folder}"
     docker push "$image_tag"
 done
