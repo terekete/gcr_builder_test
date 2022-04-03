8528a509bfff7030a94448bd6a30aaf7492c4db6
terekete <mark.gates@telus.com>
2022-04-02 22:15:35 -0400

    chore: update

--- terraform/triggers.tf
+++ terraform/triggers.tf
@@ -38,10 +38,3 @@ resource "google_cloudbuild_trigger" "image_push" {
     }
   }
 }
-
-resource "google_storage_bucket" "diff_bucket" {
-  project                     = var.project_id
-  name                        = format("rc-diffs-%s", var.project_id)
-  uniform_bucket_level_access = true
-  location                    = var.region
-}
