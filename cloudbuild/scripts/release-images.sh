d34c85b88d5733d02da12ef2fa0022ad4c002bdc
terekete <mark.gates@telus.com>
2022-04-04 23:59:47 -0400

    chore: update

--- cloudbuild/scripts/release-images.sh
+++ cloudbuild/scripts/release-images.sh
@@ -4,7 +4,7 @@ set -e
 
 cat DIFF_IMAGES.txt | while read image_folder; do
     version=$(cat "./images/$image_folder/VERSION")
-    old_image_tag="${CONTAINER_IMAGE_REGISTRY}/"${image_folder}":rc"
+    old_image_tag="${CONTAINER_IMAGE_REGISTRY}/"${image_folder}"-rc"
     new_image_tag="${CONTAINER_IMAGE_REGISTRY}/"${image_folder}":${version}"
     latest_image_tag="${CONTAINER_IMAGE_REGISTRY}/"${image_folder}":latest"
     gcloud container images add-tag "${old_image_tag}" "${new_image_tag}" "${latest_image_tag}" --quiet
