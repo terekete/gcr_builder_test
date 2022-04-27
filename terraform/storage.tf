6279a98ae717557a68ec4a62a37ec437f91c9812
terekete <mark.gates@telus.com>
2022-04-27 13:36:52 -0400

    chore: update

--- terraform/storage.tf
+++
@@ -1,7 +0,0 @@
-
-resource "google_storage_bucket" "diff_bucket" {
-  project                     = var.project_id
-  name                        = format("rc-diffs-%s", var.project_id)
-  uniform_bucket_level_access = true
-  location                    = var.region
-}
